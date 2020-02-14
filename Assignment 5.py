930471b8ec7015fe93a8e988691a14361f7a83ce
tsrsubbulxmi <60135550+tsrsubbulxmi@users.noreply.github.com>
2020-02-14 14:32:10 +0530

    Update Assignment 5.py

--- Assignment 5.py
+++ Assignment 5.py
@@ -1,3 +1,5 @@
+#1
+
 
 #2
 t=[(),(1,2,3),('a','b'),(''),('','')]
@@ -5,6 +7,13 @@ for i in t:
     pass
     if len(i)==0 and '' not in i:
         print(i,'is an empty set')
+    
+#3
+
+#4
+x=input('Enter the statement')
+print(x.title())
+
 #5
 import re
 txt='12.254.259 0.0.0 49.174.165'
@@ -14,4 +23,16 @@ for i in txt:
     op=re.findall(r'([0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9]\.[0-2]?[0-9]?[0-9])',i)
     #\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|2[0-5][0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|2[0-5][0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|2[0-5][0-5])
     print(op)
-    
+ 
+#6
+
+l=[]
+while True:
+    x=input('If you want to end giving inputs give done:')
+    if x=='done':
+        break
+    else:
+        l.append(x)
+s=' '.join(l)
+print(s)
+
