f67093e1f9330949d4e043467c5096387fd16aa1
tsrsubbulxmi <60135550+tsrsubbulxmi@users.noreply.github.com>
2020-02-16 19:59:27 +0530

    Update Assignment 5.py

--- Assignment 5.py
+++ Assignment 5.py
@@ -1,5 +1,28 @@
 #1
-
+a=input()
+x=''
+l=[]
+for i in range(0,len(a)-1):		
+    if ord(a[i+1])-ord(a[i])==0:		#checks i the characters next to each other
+        if i!=len(a)-2:
+            x=x+a[i]
+        else:
+            x=x+a[i]
+            l.append(x)
+    else:
+        x=x+a[i]
+        l.append(x)
+        x=''
+if ord(a[i])-ord(a[i+1])==0:        #condition for the last character
+    l[len(l)-1]=l[len(l)-1]+a[i+1] 
+else:
+    l.append(a[i+1])
+cnt=[]
+for i in range(0,len(l)):			#gets the length of each element 
+    cnt.append(len(l[i]))
+for i in range(len(l)):
+    print(l[i],end='')
+    print(cnt[i],end='')
 
 #2
 t=[(),(1,2,3),('a','b'),(''),('','')]
